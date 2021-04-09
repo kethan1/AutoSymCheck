5f904d8b12e2f047e47c3d98c0f957d200245ced
Kethan Vegunta <kethan@vegunta.com>
2021-04-09 09:45:56 -0700

    redirect to https

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ import os
 
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    if 'DYNO' in os.environ:
+        if request.url.startswith('http://'):
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=not('DYNO' in os.environ), host="0.0.0.0")
+    app.run(debug=not('DYNO' in os.environ))
